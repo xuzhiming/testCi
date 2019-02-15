94eb03bcf806e3618313efa4509243f4e460a6bb
xzming <xzm@umshz.com>
2019-02-15 14:23:46 +0800

    test pod

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'testCi' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+    	platform :ios, '10.0'
+
+	use_frameworks!
+
+  # Pods for testCi
+
+    	pod 'Masonry', '~> 1.1.0'
+	
+
+  target 'testCiTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- testCi/ViewController.m
+++ testCi/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Masonry/Masonry.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self testCode];
+}
+
+-(void)testCode{
+    UIView *lv = [UIView new];
+    lv.backgroundColor = [UIColor redColor];
+    UIView *pv = [UIView new];
+    pv.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:pv];
+    [pv mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+    [self.view layoutIfNeeded];
+    [pv addSubview:lv];
+    [lv mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(pv.mas_centerX).dividedBy(2);
+        make.centerY.equalTo(pv);
+        make.size.mas_equalTo(CGSizeMake(100, 100));
+    }];
+    
+    UIView *rv = [UIView new];
+    rv.backgroundColor = [UIColor greenColor];
+    [pv addSubview:rv];
+    [rv mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(pv.mas_centerX).multipliedBy(1.5);
+        make.centerY.equalTo(pv);
+        make.size.mas_equalTo(CGSizeMake(100, 100));
+    }];
+    
+    
 }
 
 
